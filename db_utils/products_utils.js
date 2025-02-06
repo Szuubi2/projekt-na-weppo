77ad5afb7711ed4e8a69e02fcc1218a77fa3ff50
lidiapodoluk <lidiapodoluk@gmail.com>
2025-02-06 16:37:38 +0100

    add inserOrder and insertOrderDetails

--- db_utils/products_utils.js
+++ db_utils/products_utils.js
@@ -48,3 +48,41 @@ export const getProductById = async (productId) => {
         throw err;
     }
 };
+
+
+
+export const insertOrder = async (userId, status) => {
+    const query = `
+        INSERT INTO Orders (UserId, Status)
+        VALUES ($1, $2)
+        RETURNING Id;
+    `;
+
+    const values = [userId, status];
+
+    try {
+        const res = await pool.query(query, values);
+        return res.rows[0].id;  // returns the id of the new order
+    } catch (err) {
+        console.error('Błąd podczas zapisywania zamówienia:', err.message);
+        throw err;
+    }
+};
+
+
+
+export const insertOrderDetails = async (orderId, productId, quantity, unitPrice) => {
+    const query = `
+        INSERT INTO OrderDetails (OrderId, ProductId, Quantity, UnitPrice)
+        VALUES ($1, $2, $3, $4);
+    `;
+
+    const values = [orderId, productId, quantity, unitPrice];
+
+    try {
+        await pool.query(query, values);
+    } catch (err) {
+        console.error('Błąd podczas zapisywania szczegółów zamówienia:', err.message);
+        throw err;
+    }
+};
